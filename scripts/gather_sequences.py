e2806bad81be601aa1062cc16f6767cd14290978
carrascomj <carrascomurielj@gmail.com>
2022-06-14 13:56:03 +0200

    feat: fetch upstreams from upstream

--- scripts/gather_sequences.py
+++ scripts/gather_sequences.py
@@ -1,3 +1,41 @@
 """Script to fetch the sequences given by an imodulon."""
 
+import sys
 import pandas as pd
+import gzip
+import typer
+
+from Bio.SeqIO.FastaIO import SimpleFastaParser
+
+
+def fetch_upstream(
+    genome: str, reverse: bool, position: int, upstream_len: int = 200
+) -> str:
+    slice = genome[max(position - upstream_len, 0) : position]
+    if reverse:
+        slice = slice[::-1]
+    return slice
+
+
+def read_genome(path: str):
+    """Read a gzip genome (assuming only/first in fasta)."""
+    opener = gzip.open if path.endswith("gz") else open
+    flags = "rt" if path.endswith("gz") else "r"
+    with opener(sys.argv[2], flags) as f:
+        return next(seq for _, seq in SimpleFastaParser(f))
+
+
+def run(input_csv: str, genome_fasta_gz: str, out_csv: str):
+    """Transform the input form process_imodulons.py to sequences."""
+    df = pd.read_csv(input_csv, index_col=0)
+    genome = read_genome(genome_fasta_gz)
+    df["seq"] = df.apply(
+        lambda x: fetch_upstream(genome, x.reverse, x.position, 200), axis=1
+    )
+    df["geneid"] = df.index
+    df.to_csv(out_csv, index=False)
+
+
+if __name__ == "__main__":
+    # the input is the output of scripts/process_imodulons.py
+    typer.run(run)
