07666948f077022ce525002d642fb66a458e2610
carrascomj <carrascomurielj@gmail.com>
2022-06-15 16:10:24 +0200

    refactor: move functions out of the function :recycle:

--- scripts/gibbssampler_function.py
+++ scripts/gibbssampler_function.py
@@ -6,15 +6,197 @@ from time import time
 import typer
 
 
-# ## DEFINE THE PATH TO YOUR COURSE DIRECTORY
+def initialize_matrix(core_len, alphabet):
+    init_matrix = [0] * core_len
+    for i in range(0, core_len):
+        row = {}
+        for letter in alphabet:
+            row[letter] = 0.0
+        init_matrix[i] = row
+    return init_matrix
 
-# In[37]:
+
+def put_to_zero(matrix):
+    for i in range(0, len(matrix)):
+        for key in matrix[i].keys():
+            matrix[i][key] = 0.0
+    return matrix
+
+
+def get_log_odds(
+    peptides,
+    alphabet,
+    bg,
+    scoring_scheme,
+    core_len,
+    c_matrix,
+    f_matrix,
+    g_matrix,
+    p_matrix,
+    w_matrix,
+    sequence_weighting,
+    beta,
+):
+    # Amino Acid Count Matrix (c)
+    c_matrix = put_to_zero(c_matrix)
+
+    for position in range(0, core_len):
+
+        # peptides has two elements; element[0] is the peptide sequence, element [1] is the core location
+        for element in peptides:
+
+            peptide = element[0]
+
+            core_start = element[1]
+            peptide, core_start = adjust_to_complementary(peptide, core_start)
+
+            c_matrix[position][peptide[core_start + position]] += 1
+
+    # Sequence Weighting
+    weights = {}
+
+    for element in peptides:
+
+        peptide = element[0]
+        core_start = element[1]
+        peptide, core_start = adjust_to_complementary(peptide, core_start)
+
+        # apply sequence weighting
+        if sequence_weighting:
+            w = 0.0
+            neff = 0.0
+
+            for position in range(0, core_len):
+                r = 0
+
+                for letter in alphabet:
+                    if c_matrix[position][letter] != 0:
+                        r += 1
+
+                s = c_matrix[position][peptide[core_start + position]]
+                w += 1.0 / (r * s)
+                neff += r
+
+            neff = neff / core_len
+
+        # do not apply sequence weighting
+        else:
+            w = 1
+            neff = len(peptides)
+
+        weights[peptide] = w
+
+    # Observed Frequencies Matrix (f)
+    f_matrix = put_to_zero(f_matrix)
+
+    for position in range(0, core_len):
+        n = 0
+
+        for element in peptides:
+            peptide = element[0]
+            core_start = element[1]
+            peptide, core_start = adjust_to_complementary(peptide, core_start)
+            f_matrix[position][peptide[core_start + position]] += weights[peptide]
+            n += weights[peptide]
+
+        for letter in alphabet:
+            f_matrix[position][letter] = f_matrix[position][letter] / n
+
+    # Pseudo Frequencies Matrix (g)
+    g_matrix = put_to_zero(g_matrix)
+
+    for position in range(0, core_len):
+        for letter_1 in alphabet:
+            for letter_2 in alphabet:
+                g_matrix[position][letter_1] += (
+                    f_matrix[position][letter_2]
+                    * scoring_scheme[letter_1][letter_2]
+                )
+
+    # Combined Frequencies Matrix (p)
+
+    alpha = neff - 1
+
+    for position in range(0, core_len):
+
+        for letter in alphabet:
+
+            num = (
+                alpha * f_matrix[position][letter]
+                + beta * g_matrix[position][letter]
+            )
+            den = alpha + beta
+            p_matrix[position][letter] = num / den
+
+    # Log Odds Weight Matrix (w)
+    for position in range(0, core_len):
+
+        for letter in alphabet:
+
+            if p_matrix[position][letter] != 0:
+
+                w_matrix[position][letter] = np.log(
+                    p_matrix[position][letter] / bg[letter]
+                ) / np.log(2)
+
+    # Calculate the overall score of the peptides to the LO matrix
+    _sum = 0
+    for position in range(0, core_len):
+        for letter in alphabet:
+            _sum += f_matrix[position][letter] * w_matrix[position][letter]
+
+    return w_matrix, _sum, p_matrix
+
+def score_peptide(peptide, core_start, core_len, matrix):
+    acum = 0
+    for i in range(0, core_len):
+        acum += matrix[i][peptide[i + core_start]]
+    return acum
 
 
 def adjust_to_complementary(candidate: str, start: int) -> tuple[str, int]:
     if start >= len(candidate) - start:
         return candidate[::-1], start - len(candidate)
     return candidate, start
+
+
+def make_background_freq_vector(GC):
+    #  ‘’'A T G C’’'
+    A = (1 - (GC)) / 2
+    T = (1 - (GC)) / 2
+    G = GC / 2
+    C = GC / 2
+    return np.array([A, T, G, C]) 
+
+
+def load_peptide_data(peptides_list: list[str], core_len: int):
+    raw_peptides = [x.upper() for x in peptides_list]
+    # only keep peptides with length equal to or longer than core_len
+    peptides = []
+    for i in range(0, len(raw_peptides)):
+        if len(raw_peptides[i]) >= core_len:
+            peptides.append(raw_peptides[i])
+        else:
+            print("Peptide length too short discard", raw_peptides[i])
+
+    peptides = sorted(peptides, key=len)
+    min_pep_len = len(peptides[0])
+    max_pep_len = len(peptides[-1])
+
+    # random core start
+    np.random.shuffle(peptides)
+    cores_start = [0] * len(peptides)
+
+    for i in range(0, len(cores_start)):
+        if len(peptides[i]) != core_len:
+            min_core_start = 0
+            # Note bug in code, corrected to +1, 9/6/2021
+            max_core_start = len(peptides[i]) - core_len + 1
+            cores_start[i] = np.random.randint(min_core_start, max_core_start)
+
+    peptides = list(zip(peptides, cores_start))
+
+    return peptides, min_pep_len, core_len
 
 
 def gibbs_sampler_dna(
@@ -42,14 +224,6 @@ def gibbs_sampler_dna(
             blosum62[letter_1][letter_2] = NTscoring[i, j]
 
     # prepare background frequencies
-    def make_background_freq_vector(GC):
-        #  ‘’'A T G C’’'
-        A = (1 - (GC)) / 2
-        T = (1 - (GC)) / 2
-        G = GC / 2
-        C = GC / 2
-        return np.array([A, T, G, C])
-
     _bg = make_background_freq_vector(GC_content)
     bg = dict(zip(alphabet, _bg))
 
@@ -57,262 +231,16 @@ def gibbs_sampler_dna(
 
     # ## Calculate log-odd matrix from a given peptide core alignment
 
-    def initialize_matrix(core_len, alphabet):
-
-        init_matrix = [0] * core_len
-
-        for i in range(0, core_len):
-
-            row = {}
-
-            for letter in alphabet:
-                row[letter] = 0.0
-
-            init_matrix[i] = row
-
-        return init_matrix
-
-    def put_to_zero(matrix):
-
-        for i in range(0, len(matrix)):
-
-            for key in matrix[i].keys():
-
-                matrix[i][key] = 0.0
-
-        return matrix
-
-    def get_log_odds(
-        peptides,
-        alphabet,
-        bg,
-        scoring_scheme,
-        core_len,
-        c_matrix,
-        f_matrix,
-        g_matrix,
-        p_matrix,
-        w_matrix,
-    ):
-
-        # Amino Acid Count Matrix (c)
-
-        c_matrix = put_to_zero(c_matrix)
-
-        for position in range(0, core_len):
-
-            # peptides has two elements; element[0] is the peptide sequence, element [1] is the core location
-            for element in peptides:
-
-                peptide = element[0]
-
-                core_start = element[1]
-                peptide, core_start = adjust_to_complementary(peptide, core_start)
-
-                c_matrix[position][peptide[core_start + position]] += 1
-
-        # Sequence Weighting
-        weights = {}
-
-        for element in peptides:
-
-            peptide = element[0]
-            core_start = element[1]
-            peptide, core_start = adjust_to_complementary(peptide, core_start)
-
-            # apply sequence weighting
-            if sequence_weighting:
-
-                w = 0.0
-                neff = 0.0
-
-                for position in range(0, core_len):
-
-                    r = 0
-
-                    for letter in alphabet:
-
-                        if c_matrix[position][letter] != 0:
-
-                            r += 1
-
-                    s = c_matrix[position][peptide[core_start + position]]
-
-                    w += 1.0 / (r * s)
-
-                    neff += r
-
-                neff = neff / core_len
-
-            # do not apply sequence weighting
-            else:
-
-                w = 1
-
-                neff = len(peptides)
-
-            weights[peptide] = w
-
-        # Observed Frequencies Matrix (f)
-        f_matrix = put_to_zero(f_matrix)
-
-        for position in range(0, core_len):
-
-            n = 0
-
-            for element in peptides:
-
-                peptide = element[0]
-
-                core_start = element[1]
-
-                peptide, core_start = adjust_to_complementary(peptide, core_start)
-                f_matrix[position][peptide[core_start + position]] += weights[peptide]
-
-                n += weights[peptide]
-
-            for letter in alphabet:
-
-                f_matrix[position][letter] = f_matrix[position][letter] / n
-
-        # Pseudo Frequencies Matrix (g)
-        g_matrix = put_to_zero(g_matrix)
-
-        for position in range(0, core_len):
-
-            for letter_1 in alphabet:
-
-                for letter_2 in alphabet:
-
-                    g_matrix[position][letter_1] += (
-                        f_matrix[position][letter_2]
-                        * scoring_scheme[letter_1][letter_2]
-                    )
-
-        # Combined Frequencies Matrix (p)
-
-        alpha = neff - 1
-
-        for position in range(0, core_len):
-
-            for letter in alphabet:
-
-                num = (
-                    alpha * f_matrix[position][letter]
-                    + beta * g_matrix[position][letter]
-                )
-
-                den = alpha + beta
-
-                p_matrix[position][letter] = num / den
-
-        # Log Odds Weight Matrix (w)
-        for position in range(0, core_len):
-
-            for letter in alphabet:
-
-                if p_matrix[position][letter] != 0:
-
-                    w_matrix[position][letter] = np.log(
-                        p_matrix[position][letter] / bg[letter]
-                    ) / np.log(2)
-
-        # Calculate the overall score of the peptides to the LO matrix
-        _sum = 0
-        for position in range(0, core_len):
-            for letter in alphabet:
-                _sum += f_matrix[position][letter] * w_matrix[position][letter]
-
-        return w_matrix, _sum, p_matrix
-
-    # ## Score peptides to matrix
-
-    # In[41]:
-
-    def score_peptide(peptide, core_start, core_len, matrix):
-        acum = 0
-        for i in range(0, core_len):
-            acum += matrix[i][peptide[i + core_start]]
-        return acum
-
-    # ## Read peptides
-
-    # In[42]:
-
-    def load_peptide_data():
-
-        # Remove peptides shorter than core_len
-        raw_peptides = [x.upper() for x in peptides_list]
-        # only keep peptides with length equal to or longer than core_len
-        peptides = []
-        for i in range(0, len(raw_peptides)):
-            if len(raw_peptides[i]) >= core_len:
-                peptides.append(raw_peptides[i])
-            else:
-                print("Peptide length too short discard", raw_peptides[i])
-
-        peptides = sorted(peptides, key=len)
-        min_pep_len = len(peptides[0])
-        max_pep_len = len(peptides[-1])
-
-        # random core start
-        np.random.shuffle(peptides)
-        cores_start = [0] * len(peptides)
-
-        for i in range(0, len(cores_start)):
-
-            if len(peptides[i]) != core_len:
-
-                min_core_start = 0
-                # Note bug in code, corrected to +1, 9/6/2021
-                max_core_start = len(peptides[i]) - core_len + 1
-
-                cores_start[i] = np.random.randint(min_core_start, max_core_start)
-
-        peptides = list(zip(peptides, cores_start))
-
-        return peptides, min_pep_len, core_len
-
-    # ## Print out w-matrix in Psi_blast format
-
-    # In[43]:
-
-    # def to_psi_blast(matrix):
-    #
-    #    header = ["", "A", "R", "N", "D", "C", "Q", "E", "G", "H", "I", "L", "K", "M", "F", "P", "S", "T", "W", "Y", "V"]
-    #
-    #    print('{:>4} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8}'.format(*header))
-    #
-    #    letter_order = ["A", "R", "N", "D", "C", "Q", "E", "G", "H", "I", "L", "K", "M", "F", "P", "S", "T", "W", "Y", "V"]
-    #
-    #    for i, row in enumerate(matrix):
-    #
-    #        scores = []
-    #
-    #        scores.append(str(i+1) + " A")
-    #
-    #        for letter in letter_order:
-    #
-    #            score = row[letter]
-    #
-    #            scores.append(round(score, 4))
-    #
-    #        print('{:>4} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8} {:>8}'.format(*scores))
-
-    # ## Main loop
-
-    # In[ ]:
 
     np.random.seed(seed)
 
     core_len = 9
 
     # get peptides
-    peptides, min_pep_len, core_len = load_peptide_data()
+    peptides, _, core_len = load_peptide_data(peptides_list, core_len)
 
     print(peptides)
 
-    T_delta = (T_f - T_i) / T_steps
     T = np.linspace(T_i, T_f, T_steps)
 
     # Define number of iterations per temperature step
@@ -343,6 +271,8 @@ def gibbs_sampler_dna(
         g_matrix,
         p_matrix,
         w_matrix,
+        sequence_weighting,
+        beta,
     )
 
     # initial kld score
@@ -395,6 +325,8 @@ def gibbs_sampler_dna(
                     g_matrix,
                     p_matrix,
                     w_matrix,
+                    sequence_weighting,
+                    beta,
                 )
 
                 # score peptide against log_odds
