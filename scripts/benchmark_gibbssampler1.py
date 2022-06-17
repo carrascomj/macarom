b6e796c433ead21dcd1dd2c61ff9ba6ee705e945
viktorht <viktorht@yahoo.dk>
2022-06-17 12:36:25 +0200

    Refactor: runs different configs of gibbssampler on evaluation TFs

--- scripts/benchmark_gibbssampler1.py
+++ scripts/benchmark_gibbssampler1.py
@@ -1,3 +1,4 @@
+from ctypes import alignment
 from gibbssampler_function import run
 import pandas as pd
 import os
@@ -7,21 +8,77 @@ import os
 # loop over imodulons
 # save pssm matrixes
 
-sequence_data_file = 'data/process/gene_seqs.csv'
-eval_pssm_file = "eval/init_eval.json"
 
+def benchmark_gibbssampler(
+    tf_list: str, sequence_csv: str, output_dir: str, *args, **kwargs
+):
+    """Run through the transcription factors in tf_list with given setting for the gibbssamples. Settings are given in the kwargs"""
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
+
+    for tf in tf_list:
+        pssm_output = output_dir + "pssm_" + str(tf)
+        alignment_output = output_dir + "alignment_" + str(tf)
+
+        print(tf)
+        try:
+            run(
+                sequences_csv=sequence_csv,
+                imodulon=tf,
+                pssm_json=pssm_output,
+                alignment_output=alignment_output,
+                **kwargs
+            )
+        except IndexError:
+            print("##################### " + str(tf) + " is not found in the iModulons")
+
+
+################ INITIATION ###############################
+# Get evaluation transcription factors
+eval_pssm_file = "eval/init_eval.json"
 eval_pssm = pd.read_json(eval_pssm_file)
 tf_list = eval_pssm.columns.to_list()
-output_dir = "data/process/gibbssampler1_results/"
 
-if not os.path.isdir(output_dir):
-    os.mkdir(output_dir)
+# Get sequences data with all imodulons
+sequence_data_file = "data/process/gene_seqs.csv"
 
-for tf in tf_list[3:]:
-    output_file = output_dir + "_" + str(tf)
+################ BENCHMARK ################################
+###### CONFIG 1: Fixed core length 9 nucleotides
+# parameters
+core_min = 9
+core_max = 9
+t_steps = 200
+iters_per_point = 200
 
-    print(tf)
-    try:
-        run(sequences_csv=sequence_data_file, imodulon = tf, pssm_json=output_file)
-    except IndexError:
-        print("##################### " + str(tf) + " is not found in the iModulons")
+# output
+output_dir = "data/process/gibbssampler_config1/"
+
+benchmark_gibbssampler(
+    tf_list=tf_list,
+    sequence_csv=sequence_data_file,
+    output_dir=output_dir,
+    core_min=core_min,
+    core_max=core_max,
+    t_steps=t_steps,
+    iters_per_point=iters_per_point,
+)
+
+###### CONFIG 2: Variable core length
+# parameters
+core_min = 5
+core_max = 30
+t_steps = 200
+iters_per_point = 200
+
+# output
+output_dir = "data/process/gibbssampler_config2/"
+
+benchmark_gibbssampler(
+    tf_list=tf_list,
+    sequence_csv=sequence_data_file,
+    output_dir=output_dir,
+    core_min=core_min,
+    core_max=core_max,
+    t_steps=t_steps,
+    iters_per_point=iters_per_point,
+)
